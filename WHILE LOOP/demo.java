04696e9f1eb844c5a6e2350e0f5d09e92fc49c5e
dnyaneshagale <150069504+dnyaneshagale@users.noreply.github.com>
2024-02-21 12:52:41 +0530

    DNA

---
+++ WHILE LOOP/demo.java
@@ -0,0 +1,21 @@
+public class demo
+{
+    public static void main(String a[])
+    {
+        int i = 0 ;
+
+        while (i <= 9999) {System.out.println("Dnyanesh" + i);
+            
+           
+
+     int j = 0;
+         while (j <= 1) {
+          System.out.println("Her" + j);
+         j++;
+            }
+        i++;
+    
+}
+System.out.println("Dnyanesh and Her" + i );
+}
+}
---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello{
+
+    public static void main (String a[])
+    {
+        System.out.print("Thank You Mummy Pappa and Yash <3");
+    }
+}
